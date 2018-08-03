58d8d33aaf25d4c5a6d9a7ed6aaa9ef508230304
Tom Leonardsson <tom.leo@hotmail.com>
2018-08-03 17:44:06 +0200

    removed tile type meme

--- pairs/core/src/se/snittarna/pairs/Tile.java
+++ pairs/core/src/se/snittarna/pairs/Tile.java
@@ -6,15 +6,9 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Tile extends GameObject {
 	
-	private TileType type;
 
-	public TileType getType() {
-		return type;
-	}
-
-	public Tile(Vector2 position, TileType type) {
-		super(position, new Vector2(32, 32), new Animation(new Sprite(type.getTexture())));
-		this.type = type;
+	public Tile(Vector2 position) {
+		super(position, new Vector2(32, 32), new Animation(new Sprite()));
 	}
 	
 	public void draw(SpriteBatch batch) {
