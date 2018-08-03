73e7c3bbb80b6311faec59e2b39eed9fbe691e25
Johannes Larsson <developer.johannes.larsson@gmail.com>
2018-08-03 15:49:06 +0200

    created project

---
+++ pairs/desktop/src/se/snittarna/pairs/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package se.snittarna.pairs.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import se.snittarna.pairs.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
