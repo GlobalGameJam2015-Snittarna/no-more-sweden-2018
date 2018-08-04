e29ab286c2d566100c395b55cd0f2fd63e827f77
Johannes Larsson <developer.johannes.larsson@gmail.com>
2018-08-04 23:21:33 +0200

    fix broken push

--- pairs/core/src/se/snittarna/pairs/Powerup.java
+++ pairs/core/src/se/snittarna/pairs/Powerup.java
@@ -1,7 +1,13 @@
 package se.snittarna.pairs;
 
+import com.badlogic.gdx.math.Vector2;
+
 import se.snittarna.pairs.GameObject;
 
 public class Powerup extends GameObject {
+
+	public Powerup(Vector2 position) {
+		super(position, new Vector2(32, 32), new Animation(AssetManager.getTexture("clock"), new Vector2(32, 32)));
+	}
 
 }
